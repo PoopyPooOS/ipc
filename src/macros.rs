a98d67e923aef1f87595ec69f768cc425352d1fd
altacountbabi <altacountbabi@users.noreply.github.com>
2025-03-15 00:09:31 +0200

    Add `define_commands` macro

---
+++ src/macros.rs
@@ -0,0 +1,25 @@
+/// Helper macro to define commands to be used through IPC.
+#[macro_export]
+macro_rules! define_commands {
+    ($($command:ident($($arg:ty),*) -> $ret:ty;)*) => {
+        #[derive(Debug, serde::Deserialize, serde::Serialize)]
+        pub enum Command {
+            $($command($($arg),*)),*
+        }
+
+        #[derive(Debug, serde::Deserialize, serde::Serialize)]
+        pub enum Response {
+            $(
+                $command(
+                    define_commands!(@return_type $ret)
+                )
+            ),*
+        }
+    };
+
+    (@return_type ()) => {};
+
+    (@return_type $ret:ty) => {
+        $ret
+    };
+}
